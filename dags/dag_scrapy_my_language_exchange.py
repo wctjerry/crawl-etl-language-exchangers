112127b8093b35c2d2d8c36cb8b0e42e7fda4e82
Jerry Wu <wctjerry@gmail.com>
2022-07-21 20:11:00 +0800

    Add data quality check task

--- dags/dag_scrapy_my_language_exchange.py
+++ dags/dag_scrapy_my_language_exchange.py
@@ -1,11 +1,11 @@
 import logging
 import os
-
-import pendulum
 from datetime import timedelta
 
+import pendulum
 import constants as c
 from airflow import DAG
+from airflow.operators.sql import SQLThresholdCheckOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from operators.spider_operator import SpiderOperator
 from scrapy_utils.spiders.mylanguageexchange import MyLanguageExchangeSpider
@@ -77,6 +77,21 @@ with DAG(
         postgres_conn_id="language_exchange_conn",
     )
 
+    # Check quality task
+    check_quality_logins = SQLThresholdCheckOperator(
+        task_id="check_quality_logins",
+        sql="SELECT COUNT(1) FROM staging_changed_mle_login",
+        min_threshold="""
+                        SELECT COUNT(1)*0.9 FROM fct_mle_user_login 
+                        WHERE login_date = '{{ macros.ds_add(data_interval_start.in_timezone("Asia/Shanghai").to_date_string(), -2) }}'::DATE
+                        """,
+        max_threshold="""
+                        SELECT COUNT(1)*1.1 FROM fct_mle_user_login 
+                        WHERE login_date = '{{ macros.ds_add(data_interval_start.in_timezone("Asia/Shanghai").to_date_string(), -2) }}'::DATE
+                        """,
+        conn_id="language_exchange_conn",
+    )
+
     # Load task
     etl_dim_users = PostgresOperator(
         task_id="ETL_dim_users",
@@ -94,6 +109,7 @@ with DAG(
         scrape_data_task
         >> create_load_staging
         >> [etl_staging_dim_users, etl_staging_fct_login]
+        >> check_quality_logins
         >> etl_dim_users
         >> etl_fct_user_login
     )
